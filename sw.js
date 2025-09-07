3186914d5363dd08fafd92bcdd7e7942bd109868
Ermakov777 <ermakovnd777@gmail.com>
2025-09-07 11:00:18 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE='prip-navip-v5';
+const ASSETS=['./','./index.html','./manifest.webmanifest',
+'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request)));});
